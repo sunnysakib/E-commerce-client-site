c312473c0ef75842ee459e5ac1b4fa70e668c064
sunnysakib <sakibur.rahaman033@gmail.com>
2021-08-27 19:35:15 +0600

    add menu item

---
+++ src/pages/Homepage/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './HomePage.scss'
+import Directory from '../../component/Directory/Directory';
+
+const HomePage = () => {
+    return (
+        <div>
+            <Directory/>
+        </div>
+    );
+};
+
+export default HomePage;
