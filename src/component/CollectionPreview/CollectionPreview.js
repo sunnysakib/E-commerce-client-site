076071dfd7e59d0eb3f434dca1e0ad6fa18b7e40
sunnysakib <sakibur.rahaman033@gmail.com>
2021-09-24 11:31:09 +0600

    updated shop data with new data and then update cart reducer to count the quantity of same product

--- src/component/CollectionPreview/CollectionPreview.js
+++ src/component/CollectionPreview/CollectionPreview.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import CollectionItem from '../CollectionItem/CollectionItem';
-import './CollectionPreview.scss'
+import React from "react";
+import CollectionItem from "../CollectionItem/CollectionItem";
+import "./CollectionPreview.scss";
 
-const CollectionPreview = ({ title, items}) => {
-    return (
-        <div className="collection-preview">
-        <h1 className="title">{title}</h1>            
-        <div className="preview">
-            {
-                items.filter((item,index) => index < 4).map(({id, ...otherProps}) =>(
-                    <CollectionItem key={id} {...otherProps}></CollectionItem>
-                ))
-            }
-        </div>
-        </div>
-    );
+const CollectionPreview = ({ title, items }) => {
+  return (
+    <div className="collection-preview">
+      <h1 className="title">{title}</h1>
+      <div className="preview">
+        {items
+          .filter((item, index) => index < 4)
+          .map((item) => (
+            <CollectionItem key={item.id} item={item}></CollectionItem>
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default CollectionPreview;
--- src/component/CustomButton/CustomButton.js
+++ src/component/CustomButton/CustomButton.js
@@ -1,10 +1,17 @@
 import React from "react";
 import "./CustomButton.scss";
 
-const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => {
+const CustomButton = ({
+  children,
+  isGoogleSignIn,
+  inverted,
+  ...otherProps
+}) => {
   return (
     <button
-      className={`${isGoogleSignIn ? "sign-in-google" : ""} custom-button`}
+      className={`${inverted ? "inverted" : ""}${
+        isGoogleSignIn ? "sign-in-google" : ""
+      } custom-button`}
       {...otherProps}
     >
       {children}
